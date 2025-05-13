60eea221470f7601c1f1d6ab4a2ca3f4a6028efe
Jonas Bjørge Andersen <dev@jonasandersen.no>
2025-05-13 23:18:33 +0200

    Added cobra

--- main.go
+++ main.go
@@ -1,42 +1,10 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-type Book struct {
-	title    string
-	author   string
-	numPages int
-
-	isSaved bool
-	savedAt time.Time
-}
-
-func (book *Book) saveBook() {
-	book.isSaved = true
-	book.savedAt = time.Now()
-}
-
-func saveBook(book *Book) {
-	book.isSaved = true
-	book.savedAt = time.Now()
-
-}
+import "github.com/jonasandersen-no/cli/cmd"
 
 func main() {
-	var book = Book{
-		title:    "",
-		author:   "",
-		numPages: 0,
-		isSaved:  false,
-		savedAt:  time.Time{},
-	}
-
-	fmt.Println(book)
-
-	book.saveBook()
-
-	fmt.Println(book)
+	cmd.Execute()
 }
